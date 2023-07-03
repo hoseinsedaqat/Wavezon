2c577d20aa5ba583098547a12cd1f20c63e9c875
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-07-03 08:22:13 +0330

    fix/slider-images

--- src/data/heroSlider.js
+++ src/data/heroSlider.js
@@ -1,17 +1,27 @@
+import sliderImg_1 from './img/heroSlider/61DUO0NqyyL._SX3000_.jpg';
+
+import sliderImg_2 from './img/heroSlider/71qid7QFWJL._SX3000_.jpg';
+
+import sliderImg_3 from './img/heroSlider/81d5OrWJAkL._SX3000_.jpg';
+
+import sliderImg_4 from './img/heroSlider/71tIrZqybrL._SX3000_.jpg';
+
+import sliderImg_5 from './img/heroSlider/61jovjd+f9L._SX3000_.jpg';
+
 export const heroSlider = [
     {
-        img: 'https://m.media-amazon.com/images/I/61DUO0NqyyL._SX3000_.jpg'
+        img: sliderImg_1
     },
     {
-        img: 'https://m.media-amazon.com/images/I/71qid7QFWJL._SX3000_.jpg'
+        img: sliderImg_2
     },
     {
-        img: 'https://m.media-amazon.com/images/I/81d5OrWJAkL._SX3000_.jpg'
+        img: sliderImg_3
     },
     {
-        img: 'https://m.media-amazon.com/images/I/71tIrZqybrL._SX3000_.jpg'
+        img: sliderImg_4
     },
     {
-        img: 'https://m.media-amazon.com/images/I/61jovjd+f9L._SX3000_.jpg'
+        img: sliderImg_5
     },
 ]
