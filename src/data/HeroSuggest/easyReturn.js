a9b3935159d1cdce8afccf1306851faba41bdb45
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-07-03 08:51:28 +0330

    fix/hero-suggest-single-products

--- src/data/HeroSuggest/easyReturn.js
+++ src/data/HeroSuggest/easyReturn.js
@@ -1,4 +1,6 @@
+import easy_return_img from '../img/heroSuggest/Fuji_Dash_Returns_1x._SY304_CB432774714_.jpg';
+
 export const easyReturn = {
     title: 'Easy returns',
-    img: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Returns_1x._SY304_CB432774714_.jpg'
+    img: easy_return_img
 }
--- src/data/HeroSuggest/informationProduct.js
+++ src/data/HeroSuggest/informationProduct.js
@@ -1,5 +1,7 @@
+import information_product_img from '../img/heroSuggest/Fuji_D2_45M_en_US_1x._CB418309979_.jpg';
+
 export const informationProduct = {
     title: 'Sign in for the best experience',
     button_title: 'Sign in securely',
-    img: 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/October/Fuji_D2_45M_en_US_1x._CB418309979_.jpg'
+    img: information_product_img
 }
