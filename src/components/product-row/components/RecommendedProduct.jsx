37b76a7cde3dea7b2d01b8346a56aceab7654445
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-07-03 09:21:08 +0330

    fix/recommend-images

--- src/components/product-row/components/RecommendedProduct.jsx
+++ src/components/product-row/components/RecommendedProduct.jsx
@@ -1,3 +1,7 @@
+import recommend_1 from '@/data/img/productRow/recommended_1.jpg';
+import recommend_2 from '@/data/img/productRow/recommended_2.jpg';
+import recommend_3 from '@/data/img/productRow/recommended_3.jpg';
+import recommend_4 from '@/data/img/productRow/recommended_4.jpg';
 import { useGlobalContext } from "@/context/useContext";
 function RecommendedProdcut() {
   const { lang } = useGlobalContext();
@@ -8,13 +12,13 @@ function RecommendedProdcut() {
         <div className='column'>
           <div className='column-1'>
             <img
-              src='https://m.media-amazon.com/images/I/814XcLAe02L._AC_SY170_.jpg'
+              src={recommend_1}
               alt='Product_One'
             />
           </div>
           <div className='column-2'>
             <img
-              src='https://m.media-amazon.com/images/I/81nc9mU1XwL._AC_SY170_.jpg'
+              src={recommend_2}
               alt='Product_Two'
             />
           </div>
@@ -22,13 +26,13 @@ function RecommendedProdcut() {
         <div className='column'>
           <div className='column-1'>
             <img
-              src='https://m.media-amazon.com/images/I/71lcImJBwrS._AC_SY170_.jpg'
+              src={recommend_3}
               alt='Product_Three'
             />
           </div>
           <div className='column-2'>
             <img
-              src='https://m.media-amazon.com/images/I/81BHlUPQDhL._AC_SY170_.jpg'
+              src={recommend_4}
               alt='Product_Four'
             />
           </div>
