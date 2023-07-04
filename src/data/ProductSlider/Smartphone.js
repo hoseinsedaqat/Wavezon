793573a875473067a13c8b9b72f934b26cd90ee0
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-07-04 11:53:25 +0330

    fix/smartphone-img

--- src/data/ProductSlider/Smartphone.js
+++ src/data/ProductSlider/Smartphone.js
@@ -1,3 +1,29 @@
+import phoneone from '../img/Smartphone/phoneone.jpg';
+
+import phonetwo from '../img/Smartphone/phonetwo.jpg';
+import phonetwo_1 from '../img/Smartphone/phonetwo_1.jpg';
+import phonetwo_2 from '../img/Smartphone/phonetwo_2.jpg';
+
+import phonethree from '../img/Smartphone/phonethree.webp';
+import phonethree_1 from '../img/Smartphone/phonethree_1.jpg';
+import phonethree_2 from '../img/Smartphone/phonethree_2.jpg';
+
+import phonefour from '../img/Smartphone/phonefour.jpg';
+import phonefour_1 from '../img/Smartphone/phonefour_1.jpg';
+import phonefour_2 from '../img/Smartphone/phonefour_2.jpg';
+
+import phonefive from '../img/Smartphone/phonefive.webp';
+import phonefive_1 from '../img/Smartphone/phonefive_1.jpg';
+import phonefive_2 from '../img/Smartphone/phonefive_2.jpg';
+
+import phonesix from '../img/Smartphone/phonesix.webp';
+import phonesix_1 from '../img/Smartphone/phonesix_1.jpg';
+import phonesix_2 from '../img/Smartphone/phonesix_2.jpg';
+
+import phoneseven from '../img/Smartphone/phoneseven.jpg';
+import phoneseven_1 from '../img/Smartphone/phoneseven_1.jpg';
+import phoneseven_2 from '../img/Smartphone/phoneseven_2.jpg';
+
 export const smartPhoneProducts = {
     title: 'Smart Phone',
     products: [
@@ -28,11 +54,11 @@ export const smartPhoneProducts = {
             ],
             gallery: [
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51YUyO7TVWL._AC_SX569_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51YUyO7TVWL._AC_SX569_.jpg',
+                    original: phoneone,
+                    thumbnail: phoneone,
                 }
             ],
-            img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51YUyO7TVWL._AC_SX569_.jpg',
+            img: phoneone,
             name: 'Nothing Phone1 5G Dual 256GB 8GB RAM Factory Unlocked (GSM Only | No CDMA - not Compatible with Verizon/Sprint) – White'
         },
         // https://www.amazon.com/Nokia-G21-Dual-SIM-Unlocked-Smartphone/dp/B09SZDN5H7/ref=sr_1_1?crid=3LZLX2IY1HCR2&keywords=nokia+g21&qid=1680035643&sprefix=nokia+g%2Caps%2C393&sr=8-1
@@ -66,19 +92,19 @@ export const smartPhoneProducts = {
             ],
             gallery: [
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61R4d4PgVoL._AC_SY679_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61R4d4PgVoL._AC_SY679_.jpg',
+                    original: phonetwo,
+                    thumbnail: phonetwo,
                 },
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51SyENMge6L._AC_SY679_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51SyENMge6L._AC_SY679_.jpg',
+                    original: phonetwo_1,
+                    thumbnail: phonetwo_1,
                 },
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/517-gMF1wcL._AC_SY679_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/517-gMF1wcL._AC_SY679_.jpg',
+                    original: phonetwo_2,
+                    thumbnail: phonetwo_2,
                 }
             ],
-            img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61R4d4PgVoL._AC_SY679_.jpg',
+            img: phonetwo,
             name: 'Nokia G21 Dual 128GB 6GB RAM Factory Unlocked (GSM Only | No CDMA - not Compatible with Verizon/Sprint) - Dusk'
         },
         // https://www.amazon.com/SAMSUNG-Unlocked-Smartphone-Adaptive-Lavender/dp/B0BLP46C86/ref=sr_1_1?crid=2O047R25DCCYM&keywords=s23&qid=1680035680&sprefix=nokia+g21%2Caps%2C1279&sr=8-1
@@ -112,19 +138,19 @@ export const smartPhoneProducts = {
             ],
             gallery: [
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71GoXXLv9AL._AC_SX466_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71GoXXLv9AL._AC_SX466_.jpg',
+                    original: phonethree,
+                    thumbnail: phonethree,
                 },
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81XmKLns-jL._AC_SX466_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81XmKLns-jL._AC_SX466_.jpg',
+                    original: phonethree_1,
+                    thumbnail: phonethree_1,
                 },
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81NtZXGIg-L._AC_SX466_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81NtZXGIg-L._AC_SX466_.jpg',
+                    original: phonethree_2,
+                    thumbnail: phonethree_2,
                 }
             ],
-            img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71GoXXLv9AL._AC_SX466_.jpg',
+            img: phonethree,
             name: 'SAMSUNG Galaxy S23 Cell Phone, Factory Unlocked Android Smartphone, 256GB Storage, 50MP Camera, Night Mode, Long Battery Life, Adaptive Display, US Version, 2023, Lavender'
         },
         // https://www.amazon.com/Apple-iPhone-256GB-Deep-Purple/dp/B0BN91GD3J/ref=sr_1_3?crid=3T5UV1YXP5FS&keywords=iphone+14&qid=1680035759&sprefix=iphone+14+pro+m%2Caps%2C503&sr=8-3
@@ -158,19 +184,19 @@ export const smartPhoneProducts = {
             ],
             gallery: [
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/518bzP8VW1L._AC_SX679_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/518bzP8VW1L._AC_SX679_.jpg',
+                    original: phonefour,
+                    thumbnail: phonefour,
                 },
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51BbFCkz6sL._AC_SX679_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51BbFCkz6sL._AC_SX679_.jpg',
+                    original: phonefour_1,
+                    thumbnail: phonefour_1,
                 },
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51STBWsePzL._AC_SX679_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51STBWsePzL._AC_SX679_.jpg',
+                    original: phonefour_2,
+                    thumbnail: phonefour_2,
                 }
             ],
-            img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/518bzP8VW1L._AC_SX679_.jpg',
+            img: phonefour,
             name: 'Apple iPhone 14 Pro, 256GB, Deep Purple - Unlocked (Renewed)'
         },
         // https://www.amazon.com/ASUS-ZenFone-2400x1080-Unlocked-AI2202-16G256G-BK/dp/B0BBP4HCSN/ref=sr_1_1?keywords=asus+zenfone+9&qid=1680035816&sprefix=asus%2Caps%2C537&sr=8-1
@@ -204,19 +230,19 @@ export const smartPhoneProducts = {
             ],
             gallery: [
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71nhAXE39KL._AC_SX466_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71nhAXE39KL._AC_SX466_.jpg',
+                    original: phonefive,
+                    thumbnail: phonefive,
                 },
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71wQqKUaDdL._AC_SX466_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71wQqKUaDdL._AC_SX466_.jpg',
+                    original: phonefive_1,
+                    thumbnail: phonefive_1,
                 },
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71fAj4+tFOL._AC_SX466_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71fAj4+tFOL._AC_SX466_.jpg',
+                    original: phonefive_2,
+                    thumbnail: phonefive_2,
                 }
             ],
-            img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71nhAXE39KL._AC_SX466_.jpg',
+            img: phonefive,
             name: 'ASUS ZenFone 9 Cell Phone, 5.9” FHD+ 2400x1080 AMOLED 120Hz, IP68, 4300mAh Battery, 50MP/12MP Dual Camera 12MP Front, 16GB RAM, 256GB, 5G LTE Unlocked Dual SIM, Black, AI2202-16G256G-BK [US Version]'
         },
         // https://www.amazon.com/OPPO-Dual-SIM-Factory-Unlocked-Smartphone/dp/B082F1QKHK/ref=sr_1_2?keywords=oppo+find+n2+flip&qid=1680035903&sprefix=oppo%2Caps%2C397&sr=8-2
@@ -250,19 +276,19 @@ export const smartPhoneProducts = {
             ],
             gallery: [
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81V0m9iJY8L._AC_SX679_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81V0m9iJY8L._AC_SX679_.jpg',
+                    original: phonesix,
+                    thumbnail: phonesix,
                 },
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61TCHW+jsZL._AC_SX679_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61TCHW+jsZL._AC_SX679_.jpg',
+                    original: phonesix_1,
+                    thumbnail: phonesix_1,
                 },
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51yaMJTFZ2L._AC_SX679_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51yaMJTFZ2L._AC_SX679_.jpg',
+                    original: phonesix_2,
+                    thumbnail: phonesix_2,
                 }
             ],
-            img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81V0m9iJY8L._AC_SX679_.jpg',
+            img: phonesix,
             name: 'OPPO Find N2 Flip Dual-SIM 256GB ROM + 8GB RAM (GSM only | No CDMA) Factory Unlocked 5G Smartphone (Astral Black) - International Version'
         },
         // https://www.amazon.com/SAMSUNG-Factory-Unlocked-SM-F721U1-Graphite/dp/B0BKTWLR31/ref=sr_1_1?keywords=samsung+z+flip+4&qid=1680035941&sprefix=samsung+z+fl%2Caps%2C434&sr=8-1
@@ -296,19 +322,19 @@ export const smartPhoneProducts = {
             ],
             gallery: [
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51BpS1pE+RL._AC_SX522_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51BpS1pE+RL._AC_SX522_.jpg',
+                    original: phoneseven,
+                    thumbnail: phoneseven,
                 },
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/31R580FmhDL._AC_SX466_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/31R580FmhDL._AC_SX466_.jpg',
+                    original: phoneseven_1,
+                    thumbnail: phoneseven_1,
                 },
                 {
-                    original: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/312JqKGtmTL._AC_SX466_.jpg',
-                    thumbnail: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/312JqKGtmTL._AC_SX466_.jpg',
+                    original: phoneseven_2,
+                    thumbnail: phoneseven_2,
                 }
             ],
-            img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51BpS1pE+RL._AC_SX522_.jpg',
+            img: phoneseven,
             name: 'SAMSUNG Galaxy Z Flip 4 Factory Unlocked SM-F721U1 128GB Graphite (Renewed)'
         },
     ]
